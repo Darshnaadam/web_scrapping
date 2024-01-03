903cd6dc325d5f1bf24459fe1c3749ee589486fd
Darshna <darshna.adam@gmail.com>
2024-01-03 23:23:56 +0530

    find the price and description of the product on the web page using find function

--- find.py
+++ find.py
@@ -4,3 +4,8 @@ from bs4 import BeautifulSoup
 url = "https://webscraper.io/test-sites/e-commerce/allinone"
 
 r = requests.get(url)
+soup = BeautifulSoup(r.text,"lxml")
+price = (soup.find("h4",{"class":"float-end price card-title pull-right"}))
+print(price.string)
+desc = (soup.find("p",class_ = "description card-text"))
+print(desc.string)
